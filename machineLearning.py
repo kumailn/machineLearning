47fec6025a34e29277f00c791f3b53fccf5bb61c
Kumail <kumailmn@gmail.com>
2017-08-09 22:20:24 -0400

    -

--- machineLearning.py
+++ machineLearning.py
@@ -1,10 +1,13 @@
-import math
+import math, datetime
 import pandas as panda
 import quandl
 import numpy
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
 
+style.use("ggplot")
 ticker = "WIKI/TSLA"
 
 forecastColumn = "Adj. Close"
@@ -33,19 +36,53 @@ dataFrame["label"] = dataFrame[forecastColumn].shift(-forecastLength)
 
 # Features
 X = numpy.array(dataFrame.drop(["label"], 1))
+X = preprocessing.scale(X)
+X = X[:-forecastLength]
+X_now = X[-forecastLength:]
 
 # Labels
-y = numpy.array(dataFrame["label"])
-
-X = preprocessing.scale(X)
-X = X[:-forecastLength + 1]
 dataFrame.dropna(inplace=True)
+y = numpy.array(dataFrame["label"])
 y= numpy.array(dataFrame["label"])
 
-print(len(X), len(y))
+#dataFrame.dropna(inplace=True)
+#dataFrame.fillna(355, inplace=True)
 
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+# Set machine learning algorithm
+classifier = LinearRegression(n_jobs=-1)
+#classifier = svm.SVR()
+
+classifier.fit(X_train, y_train)
+accuracy = classifier.score(X_test, y_test)
+
+forecastSet = classifier.predict(X_now)
+
+print(len(X), len(y))
+print(accuracy)
 print(forecastLength, dataFrameLength)
 print(dataFrame.tail())
 print(dataFrame.head())
+print(forecastSet)
+
+dataFrame["Forecast"] = numpy.nan
+last_day = dataFrame.iloc[-1].name
+last_unix = last_day.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+
+for i in forecastSet:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    dataFrame.loc[next_date] = [numpy.nan for _ in range(len(dataFrame.columns)-1)] + [i]
+
+dataFrame["Adj. Close"].plot()
+dataFrame["Forecast"].plot()
+plt.legend(loc=4)
+plt.xlabel("Date")
+plt.ylabel("$")
+plt.show()
 #print(dataFrame["differencePercent"].tail())
 #print(dataFrame["dayPercentChange"].tail())
