0dfa7381de392061605992cb96448b1a0cfec423
Kumail <kumailmn@gmail.com>
2017-08-09 20:46:49 -0400

    Added second dataframe

--- machineLearning.py
+++ machineLearning.py
@@ -1,10 +1,22 @@
 import pandas as panda
 import quandl
 
-dataFrame = quandl.get("WIKI/TSLA")
+ticker = "WIKI/TSLA"
+
+dataFrame = quandl.get(ticker)
 dataFrame = dataFrame[["Adj. Open", "Adj. High", "Adj. Low", "Adj. Close", "Adj. Volume"]]
 # Calculates the difference in percent between days high and low price (ie Volatility)
 dataFrame["differencePercent"] = (dataFrame["Adj. High"] - dataFrame["Adj. Low"]) / dataFrame["Adj. Close"] * 100
+# Calculates the percent change in stock price for that day
+dataFrame["dayPercentChange"] = (dataFrame["Adj. Close"] - dataFrame["Adj. Open"]) / dataFrame["Adj. Open"] * 100
 
-print(dataFrame.tail())
-print(dataFrame["differencePercent"].tail())
+dataFrame = dataFrame[["Adj. Close", "differencePercent", "dayPercentChange", "Adj. Volume"]]
+
+dataFrame2 = dataFrame[["Adj. Close", "differencePercent", "dayPercentChange", "Adj. Volume"]]
+# Dataframe2 stores Adjusted Volume in Millions
+dataFrame2["Adj. Volume"] = dataFrame["Adj. Volume"] / 1000000
+
+print(dataFrame2.tail())
+print(dataFrame2.head())
+#print(dataFrame["differencePercent"].tail())
+#print(dataFrame["dayPercentChange"].tail())
