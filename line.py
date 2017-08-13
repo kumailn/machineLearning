0bc23457d86e2148a8db9d8895e6f51320efc88e
Kumail <kumailmn@gmail.com>
2017-08-13 17:28:19 -0400

    added coefficient of determination

--- line.py
+++ line.py
@@ -40,25 +40,39 @@ def xs(l):
     for i in range(len(l)):
         xs2 += [l[i][0]]
     return xs2
+
 def ys(l):
     xs, ys2 = [], []
     for i in range(len(l)):
         ys2 += [l[i][1]]
     return ys2
 
-def lineOfRegression(l):
+def line_of_regression(l):
     ll = []
     for i in xs(l):
         ll += [(slope(l) * i + intercept(l))]
     return ll
 
-coordinates = [[2,7],[5,13], [20, 43]]
+def squared_error(old_ys, ys_line):
+    old_ys = numpy.array(old_ys)
+    line_ys = numpy.array(ys_line)
+    return sum((line_ys - old_ys) ** 2)
 
-print(slope([[2,7],[5,13], [20, 43]]))
-print(intercept([[2,7],[5,13], [20, 43]]))
-print(xs([[2,7],[5,13], [20, 43]]))
-print(mean([1,2,3]))
-print(lineOfRegression([[2,7],[5,13], [20, 43]]))
-plt.scatter (xs([[2,7],[5,13], [20, 43]]), ys(([[2,7],[5,13], [20, 43]])))
-print(lineOfRegression(coordinates))
+def coefficient_of_determination(l):
+    y_mean_line = [mean(ys(l)) for y in ys(l)]
+    regression_squared_error = squared_error(ys(l), line_of_regression(l))
+    y_mean_squared_error = squared_error(ys(l), y_mean_line)
+    return(1 - (regression_squared_error/y_mean_squared_error))
+
+coordinates = [[2,7],[5,13], [20, 43], [-2, 213]]
+
+print(slope(coordinates))
+print(intercept(coordinates))
+print(xs(coordinates))
+print(line_of_regression(coordinates))
+plt.scatter (xs(coordinates), ys((coordinates)))
+plt.plot(xs(coordinates), line_of_regression(coordinates))
+print(line_of_regression(coordinates))
+
+print("The coefficient of determination is %" + (str(coefficient_of_determination(coordinates) * 100)))
 plt.show()
