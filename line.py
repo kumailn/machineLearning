c18a4771b161b5eca8fe94b7160c79201508371c
Kumail <kumailmn@gmail.com>
2017-08-13 17:34:17 -0400

    updated information

--- line.py
+++ line.py
@@ -10,6 +10,7 @@ def slope(l):
     return ((mx*my) - (mean(mul(xs, ys))))/((mean(xs)**2) - (mean(sql(xs))))
 
 def intercept(l):
+    # Determines the intercept of the line of best fit for a list of given x, y pairs
     xs, ys = [], []
     for i in range(len(l)):
         xs += [l[i][0]]
@@ -18,6 +19,7 @@ def intercept(l):
     return(my - slope(l)*mx)
 
 def mean(l):
+    # Finds mean of a list
     m = 0
     for i in l:
         m += i
@@ -36,29 +38,34 @@ def sql(l):
     return ll
 
 def xs(l):
+    # Extracts x points from a list of x,y pairs
     xs2, ys = [], []
     for i in range(len(l)):
         xs2 += [l[i][0]]
     return xs2
 
 def ys(l):
+    # Extracts y points from a list of x,y pairs
     xs, ys2 = [], []
     for i in range(len(l)):
         ys2 += [l[i][1]]
     return ys2
 
 def line_of_regression(l):
+    # Determines the y values for the line of best fit
     ll = []
     for i in xs(l):
         ll += [(slope(l) * i + intercept(l))]
     return ll
 
 def squared_error(old_ys, ys_line):
+    # Determines the squared error for a list of raw y points and a list of y points for the line of best fit
     old_ys = numpy.array(old_ys)
     line_ys = numpy.array(ys_line)
     return sum((line_ys - old_ys) ** 2)
 
 def coefficient_of_determination(l):
+    # Calculates the coefficient of determination for a given list of x,y pairs
     y_mean_line = [mean(ys(l)) for y in ys(l)]
     regression_squared_error = squared_error(ys(l), line_of_regression(l))
     y_mean_squared_error = squared_error(ys(l), y_mean_line)
