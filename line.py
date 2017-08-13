14d7987b2edef23097b99485eb833a2501ae98c9
Kumail <kumailmn@gmail.com>
2017-08-13 14:55:03 -0400

    Initial Commit

---
+++ line.py
@@ -0,0 +1,13 @@
+
+def slope(l):
+    # Takes in list of x, y pairs
+    xs, ys = [], []
+    for i in range(len(l)):
+        xs += [l[i][0]]
+        ys += [l[i][1]]
+    print(xs, ys)
+
+def mean(l):
+
+
+slope([[1,2],[2,2],[56,8]])
