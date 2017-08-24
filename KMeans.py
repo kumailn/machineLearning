60cb722fbf929581163817a27062b175ac352f5b
Kumail <kumailmn@gmail.com>
2017-08-23 23:54:43 -0400

    -

--- KMeans.py
+++ KMeans.py
@@ -13,6 +13,8 @@ dataFrame.convert_objects(convert_numeric=True)
 dataFrame.fillna(0, inplace=True)
 print(dataFrame.head())
 
+
+
 G = numpy.array([[1,2],[3,4]])
 for i in range(15):
     y = random.randint(1,40)
@@ -26,7 +28,17 @@ for i in range(15):
     H = numpy.array([[y, u]])
     G = numpy.concatenate((H, G))
 
-X = numpy.array([[1,1], [2,4], [3,5], [2,1], [14,22], [21,12], [17,15], [12,26], [22,11]])
+classifier = KMeans(n_clusters=2)
+classifier.fit(G)
+
+centroids = classifier.cluster_centers_
+labels = classifier.labels_
+
+
+X = numpy.array([[1,1], [2,4], [3,5], [2,1], [14,22], [21,12], [17,115], [12,26], [22,11]])
 print(G)
+
+#s = Size of
 pyplot.scatter(G[:, 0],G[:, 1], s=50,  linewidths = 5, zorder = 10)
+pyplot.scatter(centroids[:,0], centroids[:,1], marker="x", s=100, linewidths=5, zorder=10)
 pyplot.show()
